e6bbd15aa2746d27e0f79aabd0d8b5febb7b0303
Puleeno Nguyen <puleeno@gmail.com>
2022-06-11 15:56:57 +0700

    Clone function get_page_by_path and remove post type attachment

--- src/Rewrite/TaxonomyRewrite.php
+++ src/Rewrite/TaxonomyRewrite.php
@@ -56,10 +56,96 @@ class TaxonomyRewrite extends RewriteAbstract
         return false;
     }
 
+    protected function getPageByPath($page_path, $output = OBJECT, $post_type = 'page')
+    {
+        global $wpdb;
+
+        $last_changed = wp_cache_get_last_changed('posts');
+
+        $hash      = md5($page_path . serialize($post_type));
+        $cache_key = "get_page_by_path_exclude_attachment:$hash:$last_changed";
+        wp_cache_delete('get_page_by_path_exclude_attachment');
+
+        $cached    = wp_cache_get($cache_key, 'posts');
+        if (false !== $cached) {
+            // Special case: '0' is a bad `$page_path`.
+            if ('0' === $cached || 0 === $cached) {
+                return;
+            } else {
+                return get_post($cached, $output);
+            }
+        }
+
+        $page_path     = rawurlencode(urldecode($page_path));
+        $page_path     = str_replace('%2F', '/', $page_path);
+        $page_path     = str_replace('%20', ' ', $page_path);
+        $parts         = explode('/', trim($page_path, '/'));
+        $parts         = array_map('sanitize_title_for_query', $parts);
+        $escaped_parts = esc_sql($parts);
+
+        $in_string = "'" . implode("','", $escaped_parts) . "'";
+
+        if (is_array($post_type)) {
+            $post_types = $post_type;
+        } else {
+            $post_types = array( $post_type );
+        }
+
+        $post_types          = esc_sql($post_types);
+        $post_type_in_string = "'" . implode("','", $post_types) . "'";
+        $sql                 = "
+            SELECT ID, post_name, post_parent, post_type
+            FROM $wpdb->posts
+            WHERE post_name IN ($in_string)
+            AND post_type IN ($post_type_in_string)
+        ";
+
+        $pages = $wpdb->get_results($sql, OBJECT_K);
+
+        $revparts = array_reverse($parts);
+
+        $foundid = 0;
+        foreach ((array) $pages as $page) {
+            if ($page->post_name == $revparts[0]) {
+                $count = 0;
+                $p     = $page;
+
+                /*
+                 * Loop through the given path parts from right to left,
+                 * ensuring each matches the post ancestry.
+                 */
+                while (0 != $p->post_parent && isset($pages[ $p->post_parent ])) {
+                    $count++;
+                    $parent = $pages[ $p->post_parent ];
+                    if (! isset($revparts[ $count ]) || $parent->post_name != $revparts[ $count ]) {
+                        break;
+                    }
+                    $p = $parent;
+                }
+
+                if (0 == $p->post_parent && count($revparts) == $count + 1 && $p->post_name == $revparts[ $count ]) {
+                    $foundid = $page->ID;
+                    if ($page->post_type == $post_type) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        // We cache misses as well as hits.
+        wp_cache_set($cache_key, $foundid, 'posts');
+
+        if ($foundid) {
+            return get_post($foundid, $output);
+        }
+
+        return null;
+    }
+
     public function parseRequest($wp)
     {
         $error = isset($wp->query_vars['error']) && $wp->query_vars['error'] === '404';
-        if ($error || (isset($wp->query_vars['pagename']) && is_null(get_page_by_path($wp->query_vars['pagename'])))) {
+        if ($error || (isset($wp->query_vars['pagename']) && is_null($this->getPageByPath($wp->query_vars['pagename'])))) {
             $rawSlug  = $error ? $wp->request : $wp->query_vars['pagename'];
             $taxonomy = $this->matchingTaxonomyFromPageName('/' . $rawSlug);
 
